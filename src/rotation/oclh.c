c7a3ef97421957871432ce98f9d9ebdbac05a55d
Jurriaan H. Spaaks <jhspaaks@hotmail.com>
2026-09-15 08:47:24 -0400

    changed type of error status variable from int to cl_int

--- src/rotation/oclh.c
+++ src/rotation/oclh.c
@@ -6,11 +6,11 @@
 #include <string.h>           // strerror
 
 
-static void assert_err_not_nullptr (int * err, int line);
-static void platforms_check_error (cl_int status, int * err);
+static void assert_err_not_nullptr (cl_int * err, int line);
+static void platforms_check_error (cl_int status, cl_int * err);
 
 
-static void assert_err_not_nullptr (int * err, int line) {
+static void assert_err_not_nullptr (cl_int * err, int line) {
     if (err == nullptr) {
         *err = line;
         fprintf(stderr, "ERROR %d: argument `err` shouldn't be nullptr\n", *err);
@@ -19,7 +19,7 @@ static void assert_err_not_nullptr (int * err, int line) {
 }
 
 
-void OCLH_arg_create_buffer (cl_context context, size_t nbytes, cl_mem_flags flags, cl_mem * meta, int * err) {
+void OCLH_arg_create_buffer (cl_context context, size_t nbytes, cl_mem_flags flags, cl_mem * meta, cl_int * err) {
     assert_err_not_nullptr(err, __LINE__);
     if (*err) return;
 
@@ -29,7 +29,7 @@ void OCLH_arg_create_buffer (cl_context context, size_t nbytes, cl_mem_flags fla
 
 
 void OCLH_arg_create_image (cl_context context, cl_mem_flags flags, const cl_image_format * image_format,
-                                 const cl_image_desc * image_desc, cl_mem * meta, int * err) {
+                                 const cl_image_desc * image_desc, cl_mem * meta, cl_int * err) {
     assert_err_not_nullptr(err, __LINE__);
     if (*err) return;
 
@@ -43,7 +43,7 @@ void OCLH_arg_destroy (cl_mem meta) {
 }
 
 
-void OCLH_arg_enqueue_reading_buffer (cl_command_queue queue, cl_mem meta, size_t nbytes, void * arg, int * err) {
+void OCLH_arg_enqueue_reading_buffer (cl_command_queue queue, cl_mem meta, size_t nbytes, void * arg, cl_int * err) {
     assert_err_not_nullptr(err, __LINE__);
     if (*err) return;
 
@@ -61,7 +61,7 @@ void OCLH_arg_enqueue_reading_buffer (cl_command_queue queue, cl_mem meta, size_
 }
 
 
-void OCLH_arg_enqueue_reading_image (cl_command_queue queue, cl_mem meta, const cl_image_desc * desc, void * arg, int * err) {
+void OCLH_arg_enqueue_reading_image (cl_command_queue queue, cl_mem meta, const cl_image_desc * desc, void * arg, cl_int * err) {
     assert_err_not_nullptr(err, __LINE__);
     if (*err) return;
 
@@ -83,7 +83,7 @@ void OCLH_arg_enqueue_reading_image (cl_command_queue queue, cl_mem meta, const 
 }
 
 
-void OCLH_arg_enqueue_writing_buffer (cl_command_queue queue, cl_mem meta, size_t nbytes, const void * arg, int * err) {
+void OCLH_arg_enqueue_writing_buffer (cl_command_queue queue, cl_mem meta, size_t nbytes, const void * arg, cl_int * err) {
     assert_err_not_nullptr(err, __LINE__);
     if (*err) return;
 
@@ -101,7 +101,7 @@ void OCLH_arg_enqueue_writing_buffer (cl_command_queue queue, cl_mem meta, size_
 }
 
 
-void OCLH_arg_enqueue_writing_image (cl_command_queue queue, cl_mem meta, const cl_image_desc * desc, const void * arg, int * err) {
+void OCLH_arg_enqueue_writing_image (cl_command_queue queue, cl_mem meta, const cl_image_desc * desc, const void * arg, cl_int * err) {
     assert_err_not_nullptr(err, __LINE__);
     if (*err) return;
 
@@ -123,7 +123,7 @@ void OCLH_arg_enqueue_writing_image (cl_command_queue queue, cl_mem meta, const 
 }
 
 
-void OCLH_ctx_create (int ndevices, const cl_device_id * devices, cl_context * context, int * err) {
+void OCLH_ctx_create (int ndevices, const cl_device_id * devices, cl_context * context, cl_int * err) {
     assert_err_not_nullptr(err, __LINE__);
     if (*err) return;
     cl_context_properties * properties = nullptr;
@@ -138,7 +138,7 @@ void OCLH_ctx_destroy (cl_context context) {
 }
 
 
-void OCLH_dev_get (cl_device_id device, cl_device_info param, void ** value, int * err) {
+void OCLH_dev_get (cl_device_id device, cl_device_info param, void ** value, cl_int * err) {
     size_t nbytes = 0;
     *err = clGetDeviceInfo(device, param, 0, nullptr, &nbytes);
     if (*err) {
@@ -155,7 +155,7 @@ void OCLH_dev_get (cl_device_id device, cl_device_info param, void ** value, int
 }
 
 
-void OCLH_devs_count (cl_platform_id platform, int * ndevices, int * err) {
+void OCLH_devs_count (cl_platform_id platform, int * ndevices, cl_int * err) {
     assert_err_not_nullptr(err, __LINE__);
     if (*err) return;
     cl_device_type device_type = CL_DEVICE_TYPE_ALL;
@@ -163,7 +163,7 @@ void OCLH_devs_count (cl_platform_id platform, int * ndevices, int * err) {
 }
 
 
-void OCLH_devs_create (int ndevices, cl_device_id ** devices, int * err) {
+void OCLH_devs_create (int ndevices, cl_device_id ** devices, cl_int * err) {
     assert_err_not_nullptr(err, __LINE__);
     if (*err) return;
     *devices = calloc(ndevices, sizeof(cl_device_id));
@@ -187,7 +187,7 @@ void OCLH_devs_destroy (int ndevices, cl_device_id ** devices) {
 }
 
 
-void OCLH_devs_exist (int ndevices, int * err) {
+void OCLH_devs_exist (int ndevices, cl_int * err) {
     assert_err_not_nullptr(err, __LINE__);
     if (*err) return;
     if (ndevices > 0) return;
@@ -196,7 +196,7 @@ void OCLH_devs_exist (int ndevices, int * err) {
 }
 
 
-void OCLH_devs_populate (cl_platform_id platform, int ndevices, cl_device_id * devices, int * err) {
+void OCLH_devs_populate (cl_platform_id platform, int ndevices, cl_device_id * devices, cl_int * err) {
     assert_err_not_nullptr(err, __LINE__);
     if (*err) return;
     cl_device_type device_type = CL_DEVICE_TYPE_ALL;
@@ -204,7 +204,7 @@ void OCLH_devs_populate (cl_platform_id platform, int ndevices, cl_device_id * d
 }
 
 
-void OCLH_knl_create (cl_program program, char * kernel_name, cl_kernel * kernel, int * err) {
+void OCLH_knl_create (cl_program program, char * kernel_name, cl_kernel * kernel, cl_int * err) {
     assert_err_not_nullptr(err, __LINE__);
     if (*err) return;
     *kernel = clCreateKernel(program, kernel_name, err);
@@ -220,7 +220,7 @@ void OCLH_knl_destroy (cl_kernel kernel) {
 
 
 void OCLH_knl_enqueue_execution (cl_command_queue queue, cl_kernel kernel, cl_uint ndims, const size_t * global_work_size,
-                                    const size_t * local_work_size, int * err) {
+                                    const size_t * local_work_size, cl_int * err) {
     assert_err_not_nullptr(err, __LINE__);
     if (*err) return;
 
@@ -243,7 +243,7 @@ void OCLH_knl_set_arg (cl_kernel kernel, cl_uint iarg, size_t nbytes, const void
 }
 
 
-void OCLH_platforms_count (int * nplatforms, int * err) {
+void OCLH_platforms_count (int * nplatforms, cl_int * err) {
     assert_err_not_nullptr(err, __LINE__);
     if (*err) return;
     cl_int status = clGetPlatformIDs(0, nullptr, (cl_uint *) nplatforms);
@@ -251,7 +251,7 @@ void OCLH_platforms_count (int * nplatforms, int * err) {
 }
 
 
-void OCLH_platforms_create (int nplatforms, cl_platform_id ** platforms, int * err) {
+void OCLH_platforms_create (int nplatforms, cl_platform_id ** platforms, cl_int * err) {
     assert_err_not_nullptr(err, __LINE__);
     if (*err) return;
     *platforms = calloc(nplatforms, sizeof(cl_platform_id));
@@ -270,7 +270,7 @@ void OCLH_platforms_destroy (cl_platform_id ** platforms) {
 }
 
 
-void OCLH_platforms_exist (int nplatforms, int * err) {
+void OCLH_platforms_exist (int nplatforms, cl_int * err) {
     assert_err_not_nullptr(err, __LINE__);
     if (*err) return;
     if (nplatforms > 0) return;
@@ -279,7 +279,7 @@ void OCLH_platforms_exist (int nplatforms, int * err) {
 }
 
 
-void OCLH_platforms_populate (int nplatforms, cl_platform_id * platforms, int * err) {
+void OCLH_platforms_populate (int nplatforms, cl_platform_id * platforms, cl_int * err) {
     assert_err_not_nullptr(err, __LINE__);
     if (*err) return;
     cl_int status = clGetPlatformIDs((cl_uint) nplatforms, platforms, nullptr);
@@ -288,7 +288,7 @@ void OCLH_platforms_populate (int nplatforms, cl_platform_id * platforms, int * 
 
 
 void OCLH_program_create (cl_context context, int ndevices, const cl_device_id * devices, const char * filename,
-                          cl_program * program, int * err) {
+                          cl_program * program, cl_int * err) {
     assert_err_not_nullptr(err, __LINE__);
     if (*err) return;
 
@@ -362,7 +362,7 @@ void OCLH_program_destroy (cl_program program) {
 }
 
 
-void OCLH_queue_create (cl_context context, cl_device_id device, cl_command_queue * queue, int * err) {
+void OCLH_queue_create (cl_context context, cl_device_id device, cl_command_queue * queue, cl_int * err) {
     assert_err_not_nullptr(err, __LINE__);
     if (*err) return;
     cl_command_queue_properties * properties = nullptr;
@@ -375,14 +375,14 @@ void OCLH_queue_destroy (cl_command_queue queue) {
 }
 
 
-void OCLH_queue_finish (cl_command_queue queue, int * err) {
+void OCLH_queue_finish (cl_command_queue queue, cl_int * err) {
     assert_err_not_nullptr(err, __LINE__);
     if (*err) return;
     *err = clFinish(queue);
 }
 
 
-static void platforms_check_error (cl_int status, int * err) {
+static void platforms_check_error (cl_int status, cl_int * err) {
     switch (status) {
         case CL_INVALID_VALUE: {
             *err = __LINE__;
